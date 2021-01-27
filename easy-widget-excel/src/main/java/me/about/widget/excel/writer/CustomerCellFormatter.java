8cbcdeb3f6e3e4683e6c68c0dfe85ba781bdfbc4
zhangxiaohai <zhangxiaohai@tech-winning.com>
2021-01-27 11:23:25 +0800

    格式化

--- easy-widget-excel/src/main/java/me/about/widget/excel/writer/CustomerCellFormatter.java
+++ easy-widget-excel/src/main/java/me/about/widget/excel/writer/CustomerCellFormatter.java
@@ -28,13 +28,17 @@ public class CustomerCellFormatter implements CellFormatter {
 
         BigDecimal y10 = new BigDecimal(1000000000 + "");
         BigDecimal w10 = new BigDecimal(100000 + "");
+
+        BigDecimal y = new BigDecimal(1000000000 + "");
+        BigDecimal w = new BigDecimal(10000 + "");
+
         if (absVal.compareTo(y10) >= 0) {
-            BigDecimal divide = v.divide(y10,2,BigDecimal.ROUND_HALF_UP);
+            BigDecimal divide = v.divide(y,2,BigDecimal.ROUND_HALF_UP);
             return divide.toString() + "亿" + payload;
         } else if (absVal.compareTo(w10) >= 0) {
-            BigDecimal divide = v.divide(w10,2,BigDecimal.ROUND_HALF_UP);
+            BigDecimal divide = v.divide(w,2,BigDecimal.ROUND_HALF_UP);
             return divide.toString() + "万" + payload;
-        } else if (absVal.compareTo(BigDecimal.ONE) >= 0 && absVal.compareTo(new BigDecimal(10000 + "")) < 0) {
+        } else if (absVal.compareTo(BigDecimal.ONE) >= 0 && absVal.compareTo(w10) < 0) {
             return v.toString() + "" + payload;
         }
         return value.toString() + payload;

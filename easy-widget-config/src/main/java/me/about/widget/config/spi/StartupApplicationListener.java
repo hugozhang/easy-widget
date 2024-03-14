99dd9b5b29ca60141303e0d5505854a0a17c2ae3
张小海 <zhangxiaohai@tech-winning.com>
2024-03-14 13:36:28 +0800

    注释掉

--- easy-widget-config/src/main/java/me/about/widget/config/spi/StartupApplicationListener.java
+++ easy-widget-config/src/main/java/me/about/widget/config/spi/StartupApplicationListener.java
@@ -1,6 +1,5 @@
 package me.about.widget.config.spi;
 
-import me.about.widget.config.configcenter.ConfigService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
@@ -19,6 +18,6 @@ public class StartupApplicationListener implements ApplicationListener<ContextRe
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         logger.info("spring boot startup finished.");
-        ConfigService.getInstance().startSchedule();
+//        ConfigService.getInstance().startSchedule();
     }
 }
